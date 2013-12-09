09ee3c0bcdd2d6f0a37afdce2d030547499227a3
jaredculp <jculp14@gmail.com>
2013-12-09 12:24:33 -0500

    Add support for postgres

--- app/__init__.py
+++ app/__init__.py
@@ -6,7 +6,10 @@ from flask.ext.sqlalchemy import SQLAlchemy
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'app.db')
+if os.environ.get('DATABASE_URL') is None:
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'app.db')
+else:
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 app.secret_key = 'wahoowa'
 db = SQLAlchemy(app)
 
